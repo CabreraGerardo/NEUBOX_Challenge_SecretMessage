0e4aa933f1b7d37eab59ea67ab20baaee0f0e709
Gerardo Cabrera <gerardojaviercr@gmail.com>
2021-03-14 15:19:55 -0600

    Normilized message and check if contains instruction

--- MessageChallenge/src/com/challenge/message/SecretMessage.java
+++ MessageChallenge/src/com/challenge/message/SecretMessage.java
@@ -1,9 +1,61 @@
 package com.challenge.message;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class SecretMessage {
 
 	public static void main(String[] args) {
-		
+		try {
+			Scanner scanner = new Scanner(new File("D:/Mensaje.txt"));
+			String lengths = "";
+			String instruction1 = "";
+			String instruction2 = "";
+			String message = "";
+			
+			int i = 0;
+			while (scanner.hasNextLine()) {
+				i++;
+				switch(i) {
+					case 1:
+						lengths = scanner.nextLine();
+						break;
+					case 2:
+						instruction1 = scanner.nextLine();
+						break;
+					case 3:
+						instruction2 = scanner.nextLine();
+						break;
+					case 4:
+						message = scanner.nextLine();
+						break;
+				}
+				//System.out.println(scanner.nextLine());
+			}
+			scanner.close();
+			
+			message = deleteDuplicateLetters(message);
+			
+			System.out.println(message.contains(instruction1));
+			System.out.println(message.contains(instruction2));
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 
+	
+	public static String deleteDuplicateLetters(String word) {
+	    if (word.length() <= 1) {
+	        return word;
+	    }
+
+	    if (word.substring(0,1).equalsIgnoreCase(word.substring(1,2))) {
+	        return deleteDuplicateLetters(word.substring(0, 1) + word.substring(2));
+	    }
+	    else {
+	        return word.substring(0,1) + deleteDuplicateLetters(word.substring(1));
+	    }
+	}
 }
